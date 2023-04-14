03a28cc378e91518c94af62467b9ed5fe87b91bd
Oliver Köhler <olk.git@posteo.eu>
2023-04-14 15:33:00 +0200

    mark clicked area with rectangle

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import pyscreenshot as pss
+from PIL import ImageDraw
 
-from pynput.mouse import Listener
+from pynput.mouse import Listener as MouseListener, Button
 
 
 def on_click(x, y, button, pressed):
-    # capture the entire screen
-    im = pss.grab()
+    if button == Button.left and pressed:
+        im = pss.grab(backend="pil")
 
-    # save the image to a file
-    im.save('screenshot.png')
+        draw = ImageDraw.Draw(im)
+        offset = 32
+        draw.rectangle((x - offset, y - offset, x + offset, y + offset), outline="red", width=5)
+
+        im.save("screenshot.png")
 
 
 if __name__ == '__main__':
     print("Press Ctrl+C to exit")
     try:
-        with Listener(on_click=on_click) as listener:
-            listener.join()
+        with MouseListener(on_click=on_click) as ml:
+            ml.join()
     except KeyboardInterrupt:
         print("\n Good bye!")
