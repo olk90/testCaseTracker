260a7b7bd6e2501c8cb6dd5e6cf87c221e030575
Oliver Köhler <olk.git@posteo.eu>
2023-04-15 21:42:21 +0200

    write screenshots to docx file

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import os
+
+from io import BytesIO
+from docx import Document
+from docx.shared import Cm
+
 import pyscreenshot as pss
 from PIL import ImageDraw
 
@@ -12,7 +18,25 @@ def on_click(x, y, button, pressed):
         offset = 32
         draw.rectangle((x - offset, y - offset, x + offset, y + offset), outline="red", width=5)
 
-        im.save("screenshot.png")
+        im_bytes = BytesIO()
+        im.save(im_bytes, format="png")
+        im_bytes.seek(0)
+
+        directory = os.path.expanduser("~")
+        filename = "screenshots.docx"
+        file_path = os.path.join(directory, filename)
+
+        if os.path.exists(file_path):
+            doc = Document(file_path)
+        else:
+            doc = Document()
+
+        page_width = doc.sections[0].page_width.cm - doc.sections[0].left_margin.cm - doc.sections[0].right_margin.cm
+        paragraph = doc.add_paragraph()
+        run = paragraph.add_run()
+        run.add_picture(im_bytes, width=Cm(page_width))
+
+        doc.save(file_path)
 
 
 if __name__ == '__main__':
